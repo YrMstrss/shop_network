77e3f9dad3375af9b644462985d44c305f61a0df
ValeryYaku <ya.lera55555@gmail.com>
2024-01-09 22:37:15 +0300

    created command 'csu' for creating admin

---
+++ employee/management/commands/csu.py
@@ -0,0 +1,18 @@
+from django.core.management import BaseCommand
+
+from employee.models import Employee
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        admin = Employee.objects.create(
+            email='admin@admin.com',
+            first_name='Admin',
+            last_name='Shops',
+            is_staff=True,
+            is_superuser=True
+        )
+
+        admin.set_password('password')
+        admin.save()
