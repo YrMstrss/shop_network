240ca853d5bfa01e6c87d30730c5a6fbfd2047fe
ValeryYaku <ya.lera55555@gmail.com>
2024-01-09 21:54:30 +0300

    created models 'Company', 'Product' and 'Contacts'

--- network/models.py
+++ network/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    pass
+
+
+class Product(models.Model):
+    pass
+
+
+class Company(models.Model):
+    class LinkType(models.TextChoices):
+        factory = 'Factory', 'Завод'
+        company = 'Company', 'ИП'
+        retail = 'Retail', 'Розничная сеть'
+
+    link_type = models.CharField(max_length=7, choices=LinkType.choices, verbose_name='тип звена сети')
+    name = models.CharField(max_length=30, verbose_name='название')
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE, verbose_name='контакты')
+    products = models.ManyToManyField(Product, blank=True, verbose_name='продукты')
+    provider = models.ForeignKey('self', on_delete=models.CASCADE, verbose_name='поставщик', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    level = models.IntegerField(default=0, verbose_name='уровень иерархии')
+
+    def __str__(self):
+        return f'ИП "{self.name}"'
+
+    class Meta:
+        verbose_name = 'ИП'
+        verbose_name_plural = 'ИП'
