3264a924e7d094d2a56a023d6332f834fc5dba19
ValeryYaku <ya.lera55555@gmail.com>
2024-01-10 18:47:09 +0300

    created serializers for Link and Contact

---
+++ network/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+from network.models import Link, Contact
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = '__all__'
+
+
+class LinkSerializer(serializers.ModelSerializer):
+    contact = ContactSerializer()
+
+    class Meta:
+        model = Link
+        exclude = ('level', )
+
+    def create(self, validated_data):
+        contact = validated_data.pop('contact')
+
+        link = Link.objects.create(**validated_data)
+        Contact.objects.create(**contact, link=link)
+
+        return link
