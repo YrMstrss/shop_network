e11e8f2b574adc0d1479712275226f4b1bd2b972
ValeryYaku <ya.lera55555@gmail.com>
2024-01-10 18:47:31 +0300

    created generic for Link create

--- network/views.py
+++ network/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from network.serializers import LinkSerializer
+
+
+class LinkCreateAPIView(generics.CreateAPIView):
+    serializer_class = LinkSerializer
+
+    def perform_create(self, serializer):
+        link = serializer.save()
+        if link.link_type == 'Factory':
+            link.level = 0
+            link.save()
+        else:
+            if link.provider.link_type == 'Factory':
+                link.level = 1
+                link.save()
+            else:
+                link.level = 2
+                link.save()
